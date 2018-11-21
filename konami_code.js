8b32e9e9bc7c5efba9fe7f9748ffbf3750806def
monksandninjas <blackbeltdiaries@gmail.com>
2018-11-21 12:51:40 -0800

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
+let index = 0;
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//    code = [upArrow, upArrow, downArrow, downArrow, leftArrow, rightArrow, leftArrow, rightArrow, b, a]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+function init(e) {
+// Counts how many keys match code
+  const body = document.body;
+// Checks events in body
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
